968421fd28385d44521cc6d9494a67b9a2c75a3f
Dev-ismael <dev.ismael305@gmail.com>
2022-11-27 11:41:57 +0200

    Add Laborex assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,65 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <!-- Fav Icon -->
+    <link rel="icon" href={{ asset("front/images/favicon.ico") }} type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link href={{ asset("front/css/font-awesome-all.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/flaticon.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/owl.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/bootstrap.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/jquery.fancybox.min.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/animate.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/color.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/style.css") }} rel="stylesheet">
+    <link href={{ asset("front/css/responsive.css") }} rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+
+    <!-- jequery plugins -->
+    <script src={{ asset("front/js/jquery.js")}} ></script>
+    <script src={{ asset("front/js/popper.min.js")}} ></script>
+    <script src={{ asset("front/js/bootstrap.min.js")}} ></script>
+    <script src={{ asset("front/js/owl.js")}} ></script>
+    <script src={{ asset("front/js/wow.js")}} ></script>
+    <script src={{ asset("front/js/validation.js")}} ></script>
+    <script src={{ asset("front/js/jquery.fancybox.js")}} ></script>
+    <script src={{ asset("front/js/appear.js")}} ></script>
+    <script src={{ asset("front/js/isotope.js")}} ></script>
+    <script src={{ asset("front/js/jquery.nice-select.min.js")}} ></script>
+
+    <!-- main-js -->
+    <script src={{ asset("front/js/script.js")}} ></script>
+
+
 </body>
 </html>
